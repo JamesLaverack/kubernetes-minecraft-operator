33a96cdb7d75f4ccc9deca23f47ec8bb95d07cb0
James Laverack <james@jameslaverack.com>
2022-02-06 02:31:54 +0000

    Split into three tests

--- controllers/minecraftserver_controller_test.go
+++ controllers/minecraftserver_controller_test.go
@@ -24,6 +24,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/json"
 	"k8s.io/client-go/kubernetes/scheme"
 	"path/filepath"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -92,17 +93,90 @@ func setupTestingEnvironment(ctx context.Context, t *testing.T) (client.Client, 
 	}
 }
 
-func TestMinecraftServer(t *testing.T) {
+func assertEnv(t *testing.T, container corev1.Container, name, value string) {
+	require.NotNil(t, container)
+	assert.Contains(t, container.Env, corev1.EnvVar{
+		Name:  name,
+		Value: value,
+	})
+}
+
+func generateTestServer() minecraftv1alpha1.MinecraftServer {
+	return minecraftv1alpha1.MinecraftServer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test",
+			Namespace: "default",
+		},
+		Spec: minecraftv1alpha1.MinecraftServerSpec{
+			EULA:             minecraftv1alpha1.EULAAcceptanceAccepted,
+			MinecraftVersion: "1.18.1",
+			Type:             minecraftv1alpha1.ServerTypePaper,
+		},
+	}
+}
+
+func assertOwnerReference(t *testing.T, server *minecraftv1alpha1.MinecraftServer, object metav1.Object) {
+	require.Len(t, object.GetOwnerReferences(), 1)
+	or := object.GetOwnerReferences()[0]
+	assert.Equal(t, server.Name, or.Name)
+	assert.Equal(t, "MinecraftServer", or.Kind)
+	assert.Equal(t, "minecraft.jameslaverack.com/v1alpha1", or.APIVersion)
+	assert.True(t, *or.Controller)
+}
+
+func TestOpsList(t *testing.T) {
 	ctx := context.Background()
 	k8sClient, teardownFunc := setupTestingEnvironment(ctx, t)
 	defer teardownFunc()
 
-	const ns = "default"
-	// Create a Minecraft Server resource
-	pvcObj := corev1.PersistentVolumeClaim{
+	server := generateTestServer()
+	server.Spec.OpsList = []minecraftv1alpha1.Player{
+		{
+			// There is a real minecraft user with the name "testplayer", sorry!
+			Name: "testplayer",
+			UUID: "28a38b40-120c-4883-9122-61a8727ff578",
+		},
+	}
+	err := k8sClient.Create(ctx, &server)
+	require.NoError(t, err)
+
+	// TODO Find a better way to know when the reconciler is done
+	time.Sleep(time.Second * 10)
+
+	var configMap corev1.ConfigMap
+	err = k8sClient.Get(ctx, client.ObjectKeyFromObject(&server), &configMap)
+	require.NoError(t, err)
+	assertOwnerReference(t, &server, &configMap)
+	opsFile := configMap.Data["ops.json"]
+	assert.NotEmpty(t, opsFile)
+
+	var parsed []struct {
+		UUID                string `json:"uuid"`
+		Name                string `json:"name"`
+		Level               int    `json:"level"`
+		BypassesPlayerLimit string `json:"bypassesPlayerLimit"`
+	}
+	err = json.Unmarshal([]byte(opsFile), &parsed)
+	t.Log(parsed)
+	require.NoError(t, err)
+	require.Len(t, parsed, 1)
+	assert.Equal(t, "28a38b40-120c-4883-9122-61a8727ff578", parsed[0].UUID)
+	assert.Equal(t, "testplayer", parsed[0].Name)
+	assert.Equal(t, 4, parsed[0].Level)
+	assert.Equal(t, "false", parsed[0].BypassesPlayerLimit)
+
+	// TODO Assert that we're both mounting the configmap, and that the ENV for this file is set
+}
+
+func TestMountedPVC(t *testing.T) {
+	ctx := context.Background()
+	k8sClient, teardownFunc := setupTestingEnvironment(ctx, t)
+	defer teardownFunc()
+
+	pvc := corev1.PersistentVolumeClaim{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "testpvc",
-			Namespace: ns,
+			Namespace: "default",
 		},
 		Spec: corev1.PersistentVolumeClaimSpec{
 			AccessModes: []corev1.PersistentVolumeAccessMode{
@@ -115,58 +189,42 @@ func TestMinecraftServer(t *testing.T) {
 			},
 		},
 	}
-	err := k8sClient.Create(ctx, &pvcObj)
+	err := k8sClient.Create(ctx, &pvc)
 	require.NoError(t, err)
 
-	serverObj := minecraftv1alpha1.MinecraftServer{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "test",
-			Namespace: ns,
-		},
-		Spec: minecraftv1alpha1.MinecraftServerSpec{
-			EULA:             minecraftv1alpha1.EULAAcceptanceAccepted,
-			MinecraftVersion: "1.18.1",
-			Type:             minecraftv1alpha1.ServerTypePaper,
-			World: &minecraftv1alpha1.WorldSpec{
-				PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
-					ClaimName: pvcObj.Name,
-				},
-			},
-			OpsList: []minecraftv1alpha1.Player{
-				{
-					// There is a real minecraft user with the name "testplayer", sorry!
-					Name: "testplayer",
-					UUID: "28a38b40-120c-4883-9122-61a8727ff578",
-				},
-			},
+	server := generateTestServer()
+	server.Spec.World = &minecraftv1alpha1.WorldSpec{
+		PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+			ClaimName: pvc.Name,
 		},
 	}
-	err = k8sClient.Create(ctx, &serverObj)
+	err = k8sClient.Create(ctx, &server)
 	require.NoError(t, err)
 
 	// TODO Find a better way to know when the reconciler is done
 	time.Sleep(time.Second * 10)
 
-	assertOwnerReference := func(object metav1.Object) {
-		require.Len(t, object.GetOwnerReferences(), 1)
-		or := object.GetOwnerReferences()[0]
-		assert.Equal(t, serverObj.Name, or.Name)
-		assert.Equal(t, "MinecraftServer", or.Kind)
-		assert.Equal(t, "minecraft.jameslaverack.com/v1alpha1", or.APIVersion)
-		assert.True(t, *or.Controller)
-	}
-
-	// We expect the server to create a few things
-	var configMap corev1.ConfigMap
-	err = k8sClient.Get(ctx, client.ObjectKeyFromObject(&serverObj), &configMap)
+	var pod corev1.Pod
+	err = k8sClient.Get(ctx, client.ObjectKeyFromObject(&server), &pod)
 	require.NoError(t, err)
-	assertOwnerReference(&configMap)
-	assert.NotEmpty(t, configMap.Data["ops.json"])
+}
+
+func TestBasicMinecraftServer(t *testing.T) {
+	ctx := context.Background()
+	k8sClient, teardownFunc := setupTestingEnvironment(ctx, t)
+	defer teardownFunc()
+
+	server := generateTestServer()
+	err := k8sClient.Create(ctx, &server)
+	require.NoError(t, err)
+
+	// TODO Find a better way to know when the reconciler is done
+	time.Sleep(time.Second * 10)
 
 	var pod corev1.Pod
-	err = k8sClient.Get(ctx, client.ObjectKeyFromObject(&serverObj), &pod)
+	err = k8sClient.Get(ctx, client.ObjectKeyFromObject(&server), &pod)
 	require.NoError(t, err)
-	assertOwnerReference(&pod)
+	assertOwnerReference(t, &server, &pod)
 	spec := pod.Spec
 	require.NotNil(t, spec)
 
@@ -177,22 +235,15 @@ func TestMinecraftServer(t *testing.T) {
 	assert.Equal(t, "itzg/minecraft-server:2022.1.1", container.Image)
 	assert.Equal(t, "minecraft", container.Name)
 
-	assertEnv := func(name, value string) {
-		assert.Contains(t, container.Env, corev1.EnvVar{
-			Name:  name,
-			Value: value,
-		})
-	}
-
-	assertEnv("VERSION", serverObj.Spec.MinecraftVersion)
-	assertEnv("TYPE", string(serverObj.Spec.Type))
-	assertEnv("MODE", "survival")
-	assertEnv("ENABLE_ROLLING_LOGS", "true")
+	assertEnv(t, container, "VERSION", server.Spec.MinecraftVersion)
+	assertEnv(t, container, "TYPE", string(server.Spec.Type))
+	assertEnv(t, container, "MODE", "survival")
+	assertEnv(t, container, "ENABLE_ROLLING_LOGS", "true")
 
 	var service corev1.Service
-	err = k8sClient.Get(ctx, client.ObjectKeyFromObject(&serverObj), &service)
+	err = k8sClient.Get(ctx, client.ObjectKeyFromObject(&server), &service)
 	require.NoError(t, err)
-	assertOwnerReference(&service)
+	assertOwnerReference(t, &server, &service)
 
 	assert.Equal(t, corev1.ServiceTypeLoadBalancer, service.Spec.Type)
 	require.Len(t, service.Spec.Ports, 1)
