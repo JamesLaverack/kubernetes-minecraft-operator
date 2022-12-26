16db71e9b69a3f898b1a65475eea575050af75fe
James Laverack <james@laverack.dev>
2022-12-26 05:14:01 +0000

    Scaffolding controller

--- operator/src/main.rs
+++ operator/src/main.rs
@@ -1,3 +1,21 @@
+use anyhow::Result;
+use kube::{
+    client::Client,
+    runtime::controller::Action,
+};
+use std::sync::Arc;
+use tokio::time::Duration;
+use api::MinecraftServer;
+
+#[derive(Clone)]
+pub struct Context {
+    pub client: Client,
+}
+
+async fn reconcile(minecraft: Arc<MinecraftServer>, ctx: Arc<Context>) -> Result<Action> {
+    return Ok(Action::requeue(Duration::from_secs(60 * 60)));
+}
+
 fn main() {
     println!("Hello, world!");
 }
